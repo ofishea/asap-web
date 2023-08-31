2cba33616111d3bb7234e1fd7eeb15b74633d11d
Onwuka Onyemaechi <103079199+Doplersweb@users.noreply.github.com>
2023-08-31 13:49:09 +0100

    update -ony

--- src/app/home/faq/faq.component.ts
+++ src/app/home/faq/faq.component.ts
@@ -13,11 +13,8 @@ export class FaqComponent implements OnInit {
     'Are there any ongoing promotions or discounts?',
     'Is my personal and payment information secure on this platform?',
     'What is the process for tracking my order?' ,
-    'Can I sell my own products ASAP?' ,
     'Is there a customer support contact for any issues I might encounter?',
     'Are customer reviews and ratings reliable indicators of product quality?' ,
-    'Are there any restrictions on the quantity of items I can purchase?' ,
-    'Do you offer gift wrapping or personalised messages for purchases?',
     "Can I cancel an order after it'\s' been placed?",
     'Are there any rewards or loyalty programs for frequent shoppers?',
     'How can I update my delivery address or account information?',
@@ -26,22 +23,20 @@ export class FaqComponent implements OnInit {
   ];
 
   answers = [
-    'Answer 1',
-    'Answer 1',
-    'Answer 1',
-    'Answer 1',
-    'Answer 1',
-    'Answer 1',
-    'Answer 2',
-    'Answer 3',
-    'Answer 1',
-    'Answer 2',
-    'Answer 3',
-    'Answer 1',
-    'Answer 2',
-    'Answer 1',
-    'Answer 2',
-    'Answer 3',
+    'Visit the product page, click on "Write a Review", then share your thoughts with us.',
+    'Click on the "Sign Up" button, fill the form, verify email, set a good password, then start shopping.',
+    'Your Debit cards, and other digital wallets are accepted.',
+    'Check "Promotions" section for current discounts, deals, and ongoing promotions.',
+    'Stringent security measures ensure your personal and payment data protection.',
+    'Use order ID to track the status of item(s) in "Order Tracking Section".',
+    'Yes, kindly contact our customer support for assistance with any encountered issues.',
+    'Yes, customer reviews and ratings provide valuable insights into product quality.',
+    'Cancellation is possible before delivery; check "Order History" for options available.',
+    'Absolutely! We offer coupons and rewards for our loyal shoppers.',
+    'Easily update delivery/address or account information in "Account Settings" section.',
+    'Contact us if package is lost or damaged for assistance.',
+    'Yes, download our mobile app either on IOS or Google play store for a convenient shopping experience.',
+   
   ];
   constructor() { }
 
